27e612c03aee011506d178275a53fef13b419faf
EC2 Default User <ec2-user@ip-172-31-10-145.ap-northeast-1.compute.internal>
2022-09-15 13:44:41 +0000

    レイアウト修正

--- Qkyoku/config/routes.rb
+++ Qkyoku/config/routes.rb
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'public/homes#top'
-  
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   
   namespace :public do
+    get '/about'=>'homes#about'
     patch '/withdrawal'=>'users#withdrawal'
     get '/unsubscribe'=>'users#unsubscribe'
     resources :sections, only: [:show]
