a24028a89b2237fc6a4e1b8aaa9886ea888ba86b
EC2 Default User <ec2-user@ip-172-31-10-145.ap-northeast-1.compute.internal>
2022-09-27 10:57:37 +0000

    エラー文字日本語化

--- Qkyoku/db/schema.rb
+++ Qkyoku/db/schema.rb
@@ -98,7 +98,7 @@ ActiveRecord::Schema.define(version: 2022_09_25_023818) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.boolean "is_deleted", default: false, null: false
