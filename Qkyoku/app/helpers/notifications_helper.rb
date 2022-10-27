e19bb5f4a5fa6a8c2bc2073e3fe27e369ea06e8a
EC2 Default User <ec2-user@ip-172-31-10-145.ap-northeast-1.compute.internal>
2022-10-27 02:33:40 +0000

    画像サイズ変更

--- Qkyoku/app/helpers/notifications_helper.rb
+++ Qkyoku/app/helpers/notifications_helper.rb
@@ -3,14 +3,14 @@ module NotificationsHelper
   def notification_form(notification)
 	  @visiter = notification.visiter
 	  @comment = nil
-	  your_post = link_to 'あなたの投稿', public_post_path(notification), style:'font-weight: bold;'
+	  your_post = link_to 'あなたの投稿', post_path(notification), style:'font-weight: bold;'
 	  @visiter_comment = notification.comment_id
 	  case notification.action
 	    when 'favorite' then
-	      tag.a(@visiter.name, href:public_user_path(@visiter), style:'font-weight: bold;', class: 'text-dark')+'が'+tag.a('あなたのコメント',href:public_post_path(notification.comment.post), style:'font-weight: bold;', class: 'text-dark')+'にいいねしました'
+	      tag.a(@visiter.name, href:user_path(@visiter), style:'font-weight: bold;', class: 'text-dark')+'が'+tag.a('あなたのコメント',href:post_path(notification.comment.post), style:'font-weight: bold;', class: 'text-dark')+'にいいねしました'
 	    when 'comment' then
 	    	@comment = Comment.find_by(id: @visiter_comment)&.comment_body
-	    	tag.a(@visiter.name, href:public_user_path(@visiter), style:'font-weight: bold;', class: 'text-dark')+'が'+tag.a('あなたの投稿',href:public_post_path(notification.comment.post), style:'font-weight: bold;', class: 'text-dark')+'にコメントしました'
+	    	tag.a(@visiter.name, href:user_path(@visiter), style:'font-weight: bold;', class: 'text-dark')+'が'+tag.a('あなたの投稿',href:post_path(notification.comment.post), style:'font-weight: bold;', class: 'text-dark')+'にコメントしました'
 	  end
 	end
   
